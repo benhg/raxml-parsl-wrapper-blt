dfe0eca833af41d1bd1ab73e578367a73be6ee0b
Ben Glick <glick@glick.cloud>
2020-03-26 11:12:33 -0700

    Update raxml_blt.py

--- raxml_blt.py
+++ raxml_blt.py
@@ -23,3 +23,11 @@ config = Config(
 # Enable parsl logging if you want, but it prints out a lot of (useful) info
 parsl.set_stream_logger()
 parsl.load(config)
+
+
+@bash_app
+def run_raxml(mode, name, input_phylip, cores=48):
+    import random
+    random_seed = random.getrandbits(64)
+    return f"/local/cluster/bin/raxmlHPC -m {mode} -n {name} -s {input_phylip} -p {random_seed} -# 1000 -T {cores}"    
+    
